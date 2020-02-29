15fa7bdc6e646728e82554a047906f14142d8cf6
Sayali Nazare <sayali.nazare@gep.com>
2020-02-29 13:52:25 +0530

    ExcelUpload done

---
+++ solar.models/ImportExcel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace solar.models
+{
+    public class ImportExcel
+    {
+        public string fileName { get; set; }
+        public string type { get; set; }
+    }
+}
--- solar.services/ExcelService.cs
+++ solar.services/ExcelService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using OfficeOpenXml;
 using solar.generics.Providers;
 using solar.irepo;
@@ -14,44 +15,75 @@ namespace solar.services
 {
     public class ExcelService: IExcelService
     {
+        private readonly IHostingEnvironment _hostingEnvironment;
         readonly IServicesProvider<ICategoryRepo> _categoryRepo;
         readonly IHttpContextAccessor _accessor;
 
         public ExcelService(IServicesProvider<ICategoryRepo> categoryRepo,
-            IHttpContextAccessor accessor)
+            IHttpContextAccessor accessor, IHostingEnvironment hostingEnvironment)
         {
             _accessor = accessor;
             _categoryRepo = categoryRepo;
+            _hostingEnvironment = hostingEnvironment;
         }
 
-        public Feedback ImportCategoryExcel(string tenant, string fileName)
+        public Feedback ImportCategoryExcel(string tenant, ImportExcel uploadedFile)
         {
+            string rootFolder = _hostingEnvironment.WebRootPath;
             Feedback feedback = new Feedback();
-            FileInfo file = new FileInfo("C:\\ExcelDemo.xlsx");
+            FileInfo file = new FileInfo(Path.Combine(rootFolder, "uploadedFiles", @uploadedFile.fileName));
 
             using (ExcelPackage package = new ExcelPackage(file))
             {
-                ExcelWorksheet workSheet = package.Workbook.Worksheets["CATEGORY"];
+                ExcelWorksheet workSheet = package.Workbook.Worksheets[uploadedFile.type];
                 int totalRows = workSheet.Dimension.Rows;
 
-                List<Category> categoryList = new List<Category>();
-
-                for (int i = 2; i <= totalRows; i++)
+                switch(uploadedFile.type)
                 {
-                    categoryList.Add(new Category
-                    {
-                        id = int.Parse(workSheet.Cells[i, 1].Value.ToString()),
-                        name = workSheet.Cells[i, 2].Value.ToString(),
-                        isdeleted = bool.Parse(workSheet.Cells[i, 3].Value.ToString())
-                    });
+                    case "CATEGORY":
+                        feedback.data = CategoryFile(totalRows, workSheet);
+                        break;
+                    case "PRODUCTS":
+                        feedback.data = ProductFile(totalRows, workSheet);
+                        break;
                 }
-
-                //_db.Customers.AddRange(customerList);
-                //_db.SaveChanges();
-                feedback.data = categoryList;
                 return feedback;
             }
 
+        }
+
+        private List<Category> CategoryFile(int totalRows, ExcelWorksheet workSheet)
+        {
+            List<Category> categoryList = new List<Category>();
+
+            for (int i = 2; i <= totalRows; i++)
+            {
+                categoryList.Add(new Category
+                {
+                    id = int.Parse(workSheet.Cells[i, 1].Value.ToString()),
+                    name = workSheet.Cells[i, 2].Value.ToString(),
+                    isdeleted = bool.Parse(workSheet.Cells[i, 3].Value.ToString())
+                });
+            }
+
+            return categoryList;
+        }
+
+        private List<Products> ProductFile(int totalRows, ExcelWorksheet workSheet)
+        {
+            List<Products> productList = new List<Products>();
+
+            for (int i = 2; i <= totalRows; i++)
+            {
+                productList.Add(new Products
+                {
+                    id = int.Parse(workSheet.Cells[i, 1].Value.ToString()),
+                    name = workSheet.Cells[i, 2].Value.ToString(),
+                    isdeleted = bool.Parse(workSheet.Cells[i, 3].Value.ToString())
+                });
+            }
+
+            return productList;
         }
     }
 }
